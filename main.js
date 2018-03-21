1919af8f2910061b7535fee2c85f8b82e9bd702a
CamKirk <camdenkirkland@hotmail.com>
2018-03-20 20:37:50 -0500

    init commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 $.ajax({
-    url:'http://www.asterank.com//api/asterank?query={"spec":"T"}&limit=1',
+    url:'https://www.asterank.com//api/asterank?query={"spec":"T"}&limit=1',
     method:"GET"
 });
